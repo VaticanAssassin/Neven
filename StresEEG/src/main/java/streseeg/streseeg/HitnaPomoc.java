2d845b7ba40051630283fb21bfc186e35c1a8c9c
admin <>
2015-11-04 16:17:40 +0100

    {/StresEEG/src/main/java/streseeg/streseeg/HitnaPomoc.java}

--- StresEEG/src/main/java/streseeg/streseeg/HitnaPomoc.java
+++ StresEEG/src/main/java/streseeg/streseeg/HitnaPomoc.java
@@ -9,39 +9,41 @@ public class HitnaPomoc implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label(value = "Hitna pomoc ID")
-   private java.lang.Long hitnaPomocId;
-   @org.kie.api.definition.type.Label(value = "GPS Lokacija ID")
-   private java.lang.Long gpsLokacijaId;
+   @org.kie.api.definition.type.Label("GPS Lokacija ID")
+   private Integer gpsLokacijaId;
+
+   @org.kie.api.definition.type.Label(value = "Hitna pomoc FK")
+   private streseeg.streseeg.HitnaPomoc hitnaPomoc_fk;
 
    public HitnaPomoc()
    {
    }
 
-   public java.lang.Long getHitnaPomocId()
+   public streseeg.streseeg.HitnaPomoc getHitnaPomoc_fk()
    {
-      return this.hitnaPomocId;
+      return this.hitnaPomoc_fk;
    }
 
-   public void setHitnaPomocId(java.lang.Long hitnaPomocId)
+   public void setHitnaPomoc_fk(streseeg.streseeg.HitnaPomoc hitnaPomoc_fk)
    {
-      this.hitnaPomocId = hitnaPomocId;
+      this.hitnaPomoc_fk = hitnaPomoc_fk;
    }
 
-   public java.lang.Long getGpsLokacijaId()
+   public java.lang.Integer getGpsLokacijaId()
    {
       return this.gpsLokacijaId;
    }
 
-   public void setGpsLokacijaId(java.lang.Long gpsLokacijaId)
+   public void setGpsLokacijaId(java.lang.Integer gpsLokacijaId)
    {
       this.gpsLokacijaId = gpsLokacijaId;
    }
 
-   public HitnaPomoc(java.lang.Long hitnaPomocId, java.lang.Long gpsLokacijaId)
+   public HitnaPomoc(java.lang.Integer gpsLokacijaId,
+         streseeg.streseeg.HitnaPomoc hitnaPomoc_fk)
    {
-      this.hitnaPomocId = hitnaPomocId;
       this.gpsLokacijaId = gpsLokacijaId;
+      this.hitnaPomoc_fk = hitnaPomoc_fk;
    }
 
 }
